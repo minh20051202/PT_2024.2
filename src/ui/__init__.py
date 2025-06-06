b4277c44acf3a84e2c0ccf0761a758d09f4466d4
minh20051202 <minh20051202@gmail.com>
2025-06-06 16:38:12 +0700

    Third commit

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -6,7 +6,6 @@ Gói giao diện người dùng cho Hệ thống Quản lý Hóa đơn.
 
 # Đảm bảo các submodule có thể được truy cập
 from . import gui
-from . import cli
 
 # Định nghĩa các module sẽ được import khi dùng "from invoicemanager.ui import *"
-__all__ = ['gui', 'cli'] 
+__all__ = ['gui'] 
--- src/ui/cli/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Gói giao diện dòng lệnh (CLI) cho Hệ thống Quản lý Hóa đơn.
-"""
-
-from .cli import start_cli, InvoiceManagementSystem
-
-__all__ = ['start_cli', 'InvoiceManagementSystem'] 
